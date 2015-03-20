5354aa0fd01aa0aec4f5ea8f30e07fb44dad283f
Florian Wilkens <mrfloya_github@outlook.com>
2015-03-20 21:56:14 +0100

    Added some more ffi methods

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,8 @@ pub struct PamConv {
 }
 
 extern "C" {
-    /* --------------             pam_appl.h            ------------- */
+    /* ------------------------ pam_appl.h -------------------------- */
     /* -------------- The Linux-PAM Framework layer API ------------- */
-
     pub fn pam_start(service_name: *const c_char, user: *const c_char,
         pam_conversation: *const PamConv, pamh: *mut *const PamHandle) -> c_int;
 
@@ -37,4 +36,32 @@ extern "C" {
 
     /* Password Management API's */
     pub fn pam_chauthtok(pamh: *mut PamHandle, flags: c_int) -> c_int;
+    /* ------------------------ pam_appl.h -------------------------- */
+
+    /* ----------------------- _pam_types.h ------------------------- */
+    /* ---------- Common Linux-PAM application/module PI ------------ */
+    pub fn pam_set_item(pamh: *mut PamHandle, item_type: c_int, item: *const c_void) -> c_int;
+
+    pub fn pam_get_item(pamh: *const PamHandle, item_type: c_int, item: *const *const c_void) -> c_int;
+
+    pub fn pam_strerror(pamh: *mut PamHandle, errnum: c_int) -> *const c_char;
+
+    pub fn pam_putenv(pamh: *mut PamHandle, name_value: *const c_char) -> c_int;
+
+    pub fn pam_getenv(pamh: *mut PamHandle, name: *const c_char) -> *const c_char;
+
+    pub fn pam_getenvlist(pamh: *mut PamHandle) -> *const *const c_char;
+    /* ----------------------- _pam_types.h ------------------------- */
+
+    /* ----------------------- pam_modules.h ------------------------ */
+    /* -------------------- The Linux-PAM Module PI ----------------- */
+
+    /*pub fn pam_set_data(pamh: *mut PamHandle, module_data_name: *const c_char,
+        data: *mut c_void, cleanup: extern fn()  void (*cleanup)(pamh: *mut PamHandle, void *data,
+    			     int error_status));*/
+
+    pub fn pam_get_data(pamh: *const PamHandle, module_data_name: *const c_char, data: *const *const c_void);
+
+    pub fn pam_get_user(pamh: *mut PamHandle, user: *const *const c_char, prompt: *const c_char);
+    /* ----------------------- pam_modules.h ------------------------ */
 }
