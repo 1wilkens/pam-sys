b7e0534f68390ccba61cf446f9e67fd5da5cbe3a
Florian Wilkens <mrfloya_github@outlook.com>
2015-03-20 17:45:50 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+extern crate libc;
+
+use libc::{c_char, c_int};
+
+#[repr(C)]
+#[derive(Copy)]
+pub struct PamHandle;
+
+#[repr(C)]
+#[derive(Copy)]
+pub struct PamConv;
+
+extern "C" {
+    /* --------------             pam_appl.h            ------------- */
+    /* -------------- The Linux-PAM Framework layer API ------------- */
+
+    pub fn pam_start(service_name: *const c_char, user: *const c_char,
+        pam_conversation: *const PamConv, pamh: *const *mut PamHandle) -> c_int;
+
+    pub fn pam_end(pamh: *mut PamHandle, pam_status: c_int) -> c_int;
+
+    /* Authentication API's */
+    pub fn pam_authenticate(pamh: *mut PamHandle, flags: c_int) -> c_int;
+
+    pub fn pam_setcred(pamh: *mut PamHandle, flags: c_int) -> c_int;
+
+    /* Account Management API's */
+    pub fn pam_acct_mgmt(pamh: *mut PamHandle, flags: c_int) -> c_int;
+
+    /* Session Management API's */
+    pub fn pam_open_session(pamh: *mut PamHandle, flags: c_int) -> c_int;
+
+    pub fn pam_close_session(pamh: *mut PamHandle, flags: c_int) -> c_int;
+
+    /* Password Management API's */
+    pub fn pam_chauthtok(pamh: *mut PamHandle, flags: c_int) -> c_int;
+}
