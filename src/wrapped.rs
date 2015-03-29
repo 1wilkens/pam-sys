e8b432ff234a51144e69d945a6c135d3b7a6804c
Florian Wilkens <mrfloya_github@outlook.com>
2015-03-29 01:05:15 +0100

    Added some wrapped methods. Prepare 0.0.4 release

--- src/wrapped.rs
+++ src/wrapped.rs
@@ -1,4 +1,4 @@
-use libc::{c_char, c_int};
+use libc::{c_char, c_int, c_void};
 
 use types::*;
 use raw::*;
@@ -16,25 +16,35 @@ pub unsafe fn end(handle: *mut PamHandle, status: c_int) -> PamReturnCode {
 
 #[inline]
 pub unsafe fn authenticate(handle: *mut PamHandle, flags: PamFlag) -> PamReturnCode {
-    PamReturnCode::from_i32(pam_authenticate(handle, flags as i32) as i32)
+    PamReturnCode::from_i32(pam_authenticate(handle, flags as i32))
 }
 
 #[inline]
 pub unsafe fn acct_mgmt(handle: *mut PamHandle, flags: PamFlag) -> PamReturnCode {
-    PamReturnCode::from_i32(pam_acct_mgmt(handle, flags as i32) as i32)
+    PamReturnCode::from_i32(pam_acct_mgmt(handle, flags as i32))
 }
 
 #[inline]
 pub unsafe fn setcred(handle: *mut PamHandle, flags: PamFlag) -> PamReturnCode {
-    PamReturnCode::from_i32(pam_setcred(handle, flags as i32) as i32)
+    PamReturnCode::from_i32(pam_setcred(handle, flags as i32))
 }
 
 #[inline]
 pub unsafe fn open_session(handle: *mut PamHandle, flags: PamFlag) -> PamReturnCode {
-    PamReturnCode::from_i32(pam_open_session(handle, flags as i32) as i32)
+    PamReturnCode::from_i32(pam_open_session(handle, flags as i32))
 }
 
 #[inline]
 pub unsafe fn close_session(handle: *mut PamHandle, flags: PamFlag) -> PamReturnCode {
-    PamReturnCode::from_i32(pam_close_session(handle, flags as i32) as i32)
+    PamReturnCode::from_i32(pam_close_session(handle, flags as i32))
+}
+
+#[inline]
+pub unsafe fn set_item(handle: *mut PamHandle, item_type: PamItemType, item: *const c_void) -> PamReturnCode {
+    PamReturnCode::from_i32(pam_set_item(handle, item_type as i32, item))
+}
+
+#[inline]
+pub unsafe fn get_item(handle: *const PamHandle, item_type: PamItemType, item: *const *const c_void) -> PamReturnCode {
+    PamReturnCode::from_i32(pam_get_item(handle, item_type as i32, item))
 }
