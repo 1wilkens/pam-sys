7fdb9f01f25c2c49d5a06a53ea18dd05f3786ce3
Florian Wilkens <gh@1wilkens.org>
2017-02-17 19:33:39 +0100

    Updated license headers

--- src/raw.rs
+++ src/raw.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2015 Florian Wilkens
+// Copyright (C) 2015-2017 Florian Wilkens
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 // associated documentation files (the "Software"), to deal in the Software without restriction,
