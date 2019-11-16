48f0c1eb94fbeb70ae2e63be9c65c3c202c97fe1
Florian Wilkens <gh@1wilkens.org>
2019-11-16 15:46:16 +0100

    [FIX] Only generate bindings for pam_misc.h on linux

--- wrapper.h
+++ wrapper.h
@@ -2,6 +2,9 @@
 #include <security/pam_client.h>
 #include <security/pam_ext.h>
 #include <security/pam_filter.h>
-#include <security/pam_misc.h>
 #include <security/pam_modules.h>
 #include <security/pam_modutil.h>
+
+#ifdef __linux__
+#include <security/pam_misc.h>
+#endif
