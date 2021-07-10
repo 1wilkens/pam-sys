142e9f00d1a22e7de89c89f46df25821d31d0787
orvij <85481483+orvij@users.noreply.github.com>
2021-07-10 03:16:32 +0000

    Add Support for FreeBSD
    Changes to constants and includes to support FreeBSD systems

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,41 @@
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(target_os = "freebsd")]
+mod enums {
+    // PAM flags
+    pub const PAM_SILENT: libc::c_uint = 0x8000_0000;
+    pub const PAM_DISALLOW_NULL_AUTHTOK: libc::c_uint = 1;
+    pub const PAM_ESTABLISH_CRED: libc::c_uint = 1;
+    pub const PAM_DELETE_CRED: libc::c_uint = 2;
+    pub const PAM_REINITIALIZE_CRED: libc::c_uint = 4;
+    pub const PAM_REFRESH_CRED: libc::c_uint = 8;
+    pub const PAM_PRELIM_CHECK: libc::c_uint = 1;
+    pub const PAM_UPDATE_AUTHTOK: libc::c_uint = 2;
+    pub const PAM_CHANGE_EXPIRED_AUTHTOK: libc::c_uint = 4;
+    // flags not defined in FreeBSD
+    pub const PAM_CONV_AGAIN: libc::c_uint = 30;
+    pub const PAM_INCOMPLETE: libc::c_uint = 310;
+    pub const PAM_FAIL_DELAY: libc::c_uint = 10;
+    pub const PAM_XDISPLAY: libc::c_uint = 11;
+    pub const PAM_XAUTHDATA: libc::c_uint = 12;
+    pub const PAM_AUTHTOK_TYPE: libc::c_uint = 13;
+}
+
+// Non-compat Linux definitions
+// FIXME: use compat versions here?
+#[cfg(not(target_os = "freebsd"))]
+mod enums {
+    pub const PAM_SILENT: libc::c_uint = 0x8000;
+    pub const PAM_DISALLOW_NULL_AUTHTOK: libc::c_uint = 0x1;
+    pub const PAM_ESTABLISH_CRED: libc::c_uint = 0x2;
+    pub const PAM_DELETE_CRED: libc::c_uint = 0x4;
+    pub const PAM_REINITIALIZE_CRED: libc::c_uint = 0x8;
+    pub const PAM_REFRESH_CRED: libc::c_uint = 0x10;
+    pub const PAM_PRELIM_CHECK: libc::c_uint = 0x4000;
+    pub const PAM_UPDATE_AUTHTOK: libc::c_uint = 0x2000;
+    pub const PAM_CHANGE_EXPIRED_AUTHTOK: libc::c_uint = 0x20;
+}
+
+pub use enums::*;
--- wrapper.h
+++ wrapper.h
@@ -1,10 +1,17 @@
 #include <security/pam_appl.h>
+
+#ifdef __linux__
 #include <security/pam_client.h>
 #include <security/pam_ext.h>
 #include <security/pam_filter.h>
-#include <security/pam_modules.h>
 #include <security/pam_modutil.h>
-
-#ifdef __linux__
 #include <security/pam_misc.h>
+#include <security/pam_modules.h>
+#endif
+
+#ifdef __FreeBSD__
+#include <security/pam_constants.h>
+#include <security/pam_mod_misc.h>
+#include <security/pam_modules.h>
+#include <security/pam_types.h>
 #endif
