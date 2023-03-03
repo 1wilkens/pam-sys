b5d6111dcccd11f1352664dd09775083f50261f1
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-03-03 20:08:50 +0100

    Get started on better implementation handling

--- build.rs
+++ build.rs
@@ -3,21 +3,16 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    // Tell cargo to tell rustc to link the system pam
-    // shared library.
-    println!("cargo:rustc-link-lib=pam");
+const PAM_IMPL_ENV_VAR: &str = "USE_PAM_IMPL";
 
-    // pam_misc is only supported on Linux afaik
-    if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-lib=pam_misc");
-    }
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum PamImplementation {
+    LinuxPam,
+    OpenPam,
+}
 
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=wrapper.h");
-
-    // Prepare bindgen builder
-    let mut builder = bindgen::Builder::default()
+fn default_builder() -> bindgen::Builder {
+    bindgen::Builder::default()
         // Our header
         .header("wrapper.h")
         // Use libc for c-types
@@ -33,33 +28,92 @@ fn main() {
         .blocklist_function("pam_v.*")
         .blocklist_function("pam_syslog")
         .blocklist_function("pam_prompt")
+
         // Allow all PAM constants
         .allowlist_var("PAM_.*")
         // Allow all PAM functions..
         .allowlist_function("pam_.*")
-        // ..except module related functions (pam_sm_*)
-        .blocklist_function("pam_sm_.*");
+}
+
+impl PamImplementation {
+    fn resolve() -> Self {
+        if let Ok(pam_imp) = env::var(PAM_IMPL_ENV_VAR) {
+            let pam_impl = pam_imp.to_lowercase();
+            return match &pam_impl[..] {
+                "linuxpam" => Self::LinuxPam,
+                "openpam" => Self::OpenPam,
+                _ => {
+                    panic!("Unrecognized '{}' environment variable value '{}'. Assessing from other information.", PAM_IMPL_ENV_VAR, pam_impl);
+                }
+            };
+        }
+
+        // LinuxPAM is used by linux and android
+        if cfg!(target_os = "linux") || cfg!(target_os = "android") {
+            Self::LinuxPam
+        } else if cfg!(target_os = "freebsd")
+            || cfg!(target_os = "netbsd")
+            || cfg!(target_os = "macos")
+            || cfg!(target_os = "ios")
+            || cfg!(target_os = "dragonfly")
+        {
+            Self::OpenPam
+        } else {
+            panic!("Failed to resolve the PAM implementation. Use an appropriate target platform or set the `{}` environment variable to either `LINUXPAM` or `OPENPAM`.", PAM_IMPL_ENV_VAR);
+        }
+    }
+
+    fn impl_name(self) -> &'static str {
+        match self {
+            Self::LinuxPam => "linux-pam",
+            Self::OpenPam => "openpam",
+        }
+    }
+}
+
+fn main() {
+    // Tell cargo to tell rustc to link the system pam shared library.
+    println!("cargo:rustc-link-lib=pam");
+
+    let pam_implementation = PamImplementation::resolve();
+    println!(
+        "cargo:rustc-cfg=pam_sys_pam_impl=\"{impl_name}\"",
+        impl_name = pam_implementation.impl_name()
+    );
+
+    use PamImplementation::{LinuxPam, OpenPam};
+
+    // pam_misc is only supported on Linux
+    if pam_implementation == LinuxPam {
+        println!("cargo:rustc-link-lib=pam_misc");
+    }
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // Prepare bindgen builder
 
     // Platform-specific adaptions
-    if cfg!(target_os = "linux") {
-        builder = builder
-            // Set macro constants to signed int, as all functions that accept
-            // these constants use signed int as the parameter type
-            .default_macro_constant_type(bindgen::MacroTypeVariation::Signed)
-            //
-            // Use libc types so our signatures are slightly nicer
-            .raw_line("use libc::{uid_t, gid_t, group, passwd, spwd};")
-            .blocklist_type(".*gid_t")
-            .blocklist_type(".*uid_t")
-            .blocklist_type("group")
-            .blocklist_type("passwd")
-            .blocklist_type("spwd");
-    } else if cfg!(target_os = "freebsd") || cfg!(target_os = "netbsd") {
-        // XXX: this should include all OS that use openPAM
-        builder = builder
-            // Use libc types so our signatures are slightly nicer
-            .raw_line("use libc::passwd;")
-            .blocklist_type("passwd");
+    match pam_implementation {
+        LinuxPam => {
+            builder = builder
+                // Set macro constants to signed int, as all functions that accept these constants use
+                // signed int as the parameter type
+                .default_macro_constant_type(bindgen::MacroTypeVariation::Signed)
+                // Use libc types so our signatures are slightly nicer
+                .raw_line("use libc::{uid_t, gid_t, group, passwd, spwd};")
+                .blocklist_type(".*gid_t")
+                .blocklist_type(".*uid_t")
+                .blocklist_type("group")
+                .blocklist_type("passwd")
+                .blocklist_type("spwd");
+        }
+        OpenPam => {
+            builder = builder
+                // Use libc types so our signatures are slightly nicer
+                .raw_line("use libc::passwd;")
+                .blocklist_type("passwd");
+        }
     }
 
     let bindings = builder
